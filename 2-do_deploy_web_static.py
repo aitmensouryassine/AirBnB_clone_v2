1703105c4eb51585e9d4eaad42761bb2ee19fd1b
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-10-06 19:02:07 +0000

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""
-Deploy archive
-"""
+"""Deploy archive"""
 from fabric.api import put, run, local, env
 from datetime import datetime
 import os
+from fabric.decorators import runs_once
 
 
 env.user = "ubuntu"
@@ -12,13 +11,14 @@ env.key_filename = "~/.ssh/id_rsa"
 env.hosts = ['54.158.208.45', '3.85.33.151']
 
 
+@runs_once
 def do_pack():
     """packs web_static folder"""
 
     local("mkdir -p versions")
 
-    path = "versions/web_static_{}.tgz"\
-        .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S"))
+    dt = datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")
+    path = "versions/web_static_{}.tgz".format(dt)
     res = local("tar -cvzf {} web_static".format(path))
 
     if res.failed:
@@ -29,26 +29,26 @@ def do_pack():
 def do_deploy(archive_path):
     """ Deploy archive"""
 
-    if not os.path.exists(archive_path):
-        return False
-
-    filename_ext = os.path.basename(archive_path)
-    filename = filename_ext[:-4]
-
     try:
-        put(archive_path, "/tmp/")
-        run("mkdir -p /data/web_static/releases/{}".format(filename))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}"
-            .format(filename_ext, filename))
+        if not os.path.exists(archive_path):
+            return False
+
+        filename_ext = os.path.basename(archive_path)
+        filename = filename_ext[:-4]
+        releases_path = "/data/web_static/releases/"
+
+        put(archive_path, "/tmp/{}".format(filename_ext))
+        run("rm -rf {}{}".format(releases_path, filename))
+        run("mkdir -p {}{}".format(releases_path, filename))
+        run("tar -xzf /tmp/{} -C {}{}"
+            .format(filename_ext, releases_path, filename))
         run("rm /tmp/{}".format(filename_ext))
-        run("mv /data/web_static/releases/{}/web_static/*"
-            " /data/web_static/releases/{}/".format(filename, filename))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(filename))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(releases_path, filename))
+        run("rm -rf {}{}/web_static".format(releases_path, filename))
         run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(filename))
+        run("ln -s {}{}/ /data/web_static/current"
+            .format(releases_path, filename))
+        print("New version deployed!")
+        return True
     except Exception:
         return False
-
-    print("New version deployed!")
-    return True
