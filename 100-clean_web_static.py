7c97b819800ce834506ea5c2dfc926d04e743772
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-10-06 18:12:50 +0000

    Task  4

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -34,27 +34,27 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
 
-    filename_ext = os.path.basename(archive_path)
-    filename = filename_ext[:-4]
-
     try:
+        filename_ext = os.path.basename(archive_path)
+        ext_length = ".tgz".length
+        filename = filename_ext[:-ext_length]
+
         put(archive_path, "/tmp/{}".format(filename_ext))
         run("rm -rf /data/web_static/releases/{}".format(filename))
         run("mkdir -p /data/web_static/releases/{}".format(filename))
         run("tar -xzf /tmp/{} -C /data/web_static/releases/{}"
             .format(filename_ext, filename))
         run("rm /tmp/{}".format(filename_ext))
-        run("mv /data/web_static/releases/{}/web_static/*"
-            " /data/web_static/releases/{}/".format(filename, filename))
+        run("mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/".format(filename, filename))
         run("rm -rf /data/web_static/releases/{}/web_static".format(filename))
         run("rm -rf /data/web_static/current")
         run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
             .format(filename))
+        print("New version deployed!")
+        return True
     except Exception:
         return False
-
-    print("New version deployed!")
-    return True
 
 
 def deploy():
@@ -74,15 +74,8 @@ def do_clean(number=0):
     if num == 0:
         num == 1
 
-    versions = os.listdir("versions")
-    versions = sorted(versions)
-
-    for i in range(num):
-        versions.pop()
-
     with lcd("versions"):
-        for filename_ext in versions:
-            local("rm -rf ./{}".format(filename_ext))
+        local("ls -dt * | tail -n +{} | sudo xargs rm -f".format(num))
 
     with cd("/data/web_static/releases"):
         run("ls -dt * | tail -n +{} | sudo xargs rm -rf".format(num))
